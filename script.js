6b505ddaccd7997843faf08be70051b7e040dc8f
Otto D <154452516+OtDie@users.noreply.github.com>
2023-12-20 18:30:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 // This is the file with the code for my webflow website.
 // I have made a change and now this is in the edit branch.
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.4/gsap.min.js"></script>;
